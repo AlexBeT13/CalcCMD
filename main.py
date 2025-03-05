360a54d47a85df31a1fbad61fbe6380a4f395a62
AlexBeT13 <saxarixa51@gmail.com>
2025-03-05 10:21:45 +0200

    update CMD

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello")
+def calculator():
+    while True:
+        expr = input("Введите выражение ('exit' для выхода): ")
+        if expr.lower() == "exit":
+            print("Программа завершена.")
+            break
+        try:
+            result = eval(expr)
+            print(f"Результат: {result}")
+        except Exception as e:
+            print(f"Ошибка ввода: {e}")
+
+if __name__ == "__main__":
+    calculator()
